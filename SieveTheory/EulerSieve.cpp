b75e7eb37aa7e088f6a2441b59a551d3ec40b4b9
Vian Lee <paramedoct@gmail.com>
2025-01-18 19:39:00 +0900

    Euler Sieve 작업 준비

---
+++ SieveTheory/EulerSieve.cpp
@@ -0,0 +1,16 @@
+#include "EulerSieve.h"
+
+bool EulerSieve::erase_exist(std::string _max)
+{
+	return false;
+}
+
+bool EulerSieve::erase_new(std::string _max)
+{
+	return false;
+}
+
+bool EulerSieve::step(std::string _max)
+{
+	return false;
+}
